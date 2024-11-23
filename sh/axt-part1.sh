02a1293b91f99d035cb0e35f8d055a2eda7a717d
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2024-11-23 20:02:39 +0300

    Update axt-part1.sh

--- sh/axt-part1.sh
+++ sh/axt-part1.sh
@@ -38,5 +38,4 @@
 ##echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages" >> feeds.conf.default
 echo "src-git youtubeUnblock https://github.com/Waujito/youtubeUnblock.git;openwrt" >> feeds.conf.default
 echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
-echo "src-git fantastic_packages https://github.com/fantastic-packages/packages/tree/master/feeds/packages" >> feeds.conf.default
-echo "src-git fantastic_luci https://github.com/fantastic-packages/packages/tree/master/feeds/luci" >> feeds.conf.default
+echo "src-git fantastic https://github.com/fantastic-packages/packages.git" >> feeds.conf.default
