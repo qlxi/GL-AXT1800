5b66caba4be7f86a3fd10278d6fa1ade35e8e7f8
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2024-11-24 01:12:02 +0300

    Update axt-part1.sh

--- sh/axt-part1.sh
+++ sh/axt-part1.sh
@@ -36,6 +36,6 @@
 # echo "src-git iwrt_packages https://github.com/immortalwrt/packages.git" >> feeds.conf.default
 # echo "src-git iwrt_luci https://github.com/immortalwrt/luci.git" >> feeds.conf.default
 ##echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages" >> feeds.conf.default
-echo "src-git youtubeUnblock https://github.com/Waujito/youtubeUnblock.git;openwrt" >> feeds.conf.default
+### echo "src-git youtubeUnblock https://github.com/Waujito/youtubeUnblock.git;openwrt" >> feeds.conf.default
 echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
-### echo "src-git fantastic https://github.com/fantastic-packages/packages.git" >> feeds.conf.default
+echo "src-git fantastic https://github.com/fantastic-packages/packages.git" >> feeds.conf.default
